04af9f6bfb910fa6e0905a3e3f6be1b8d4764110
Jian Lik Ng <jng78@gatech.edu>
2023-11-06 00:13:42 +0000

    Added section which makes use of computed functionality in vue to count number of completed to do items

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div id="app">
     <h1>To-Do List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary">{{listSummary}}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item 
           :label="item.label" 
           :done="item.done" 
-          :id="item.id"></to-do-item>
+          :id="item.id"
+          @checkbox-changed="updateDoneStatus(item.id)"
+          ></to-do-item>
       </li>
     </ul>
   </div>
@@ -41,7 +44,18 @@ export default {
       console.log("To-do added:", toDoLabel);
       this.ToDoItems.push({id:uniqueId('todo-'), label: toDoLabel, done: false});
     },
-  }
+    updateDoneStatus(toDoId) {
+      const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId)
+      toDoToUpdate.done = !toDoToUpdate.done
+    }
+  },
+  // background functions that run computations on change
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter((item) =>item.done).length
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
+    }
+}
 
 }
 </script>
